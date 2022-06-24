ccc54e3f0fd32e69d1e02b4d6b476df7386ddbfc
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-25 00:45:23 +0530

    made the notification component

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import { SocketContext } from "../Context";
+import { useContext } from "react";
 
 const Notifications = () => {
-  return <div>Notifications</div>;
+  const socketCtx = useContext(SocketContext);
+  return (
+    <div>
+      {socketCtx.call.isReceivedCall && !socketCtx.callAccepted && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <h1> {socketCtx.call.name} is Calling :</h1>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={socketCtx.answerCall}
+          >
+            Answer
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Notifications;
