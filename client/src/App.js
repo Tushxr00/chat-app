443bb5a6068d24ded6fde9aae8d90896bc8922c8
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-24 17:17:08 +0530

    added styles to the app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
 import React from "react";
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, makeStyles, Typography } from "@material-ui/core";
 
 import Notifications from "./components/Notifications";
 import Options from "./components/Options";
 import VideoPlayer from "./components/VideoPlayer";
 
-// import AppBar from "@mui/material/AppBar";
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <AppBar position="static" color="inherit">
+    <div className={classes.wrapper}>
+      <AppBar position="static" color="inherit" className={classes.appBar}>
         <Typography variant="h2" align="center">
-          Video chat
+          Video Chat
         </Typography>
       </AppBar>
       <VideoPlayer />
