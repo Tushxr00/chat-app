859ceec35d5b9037f274f93e94b846cf95572f6c
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-24 16:45:18 +0530

    initialized the client side app

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send(`server is running on port :  ${PORT}`);
