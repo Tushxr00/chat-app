a4600863c5bd26e097753880ddb6d74a297b76fd
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-24 16:51:05 +0530

    created component layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { AppBar, Typography } from "@material-ui/core";
 import React from "react";
 
 const App = () => {
-  return <div>Video Chat App</div>;
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video chat
+        </Typography>
+      </AppBar>
+      {/* VideoPlaye */}
+      {/* Options -> Notifications */}
+    </div>
+  );
 };
 
 export default App;
