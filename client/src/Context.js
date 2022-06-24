9e3d8de905b60b592606e1e66f6b86d3824e3817
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-25 00:34:50 +0530

    completed the options component

--- client/src/Context.js
+++ client/src/Context.js
@@ -2,7 +2,22 @@ import React, { createContext, useState, useRef, useEffect } from "react";
 import { io } from "socket.io-client";
 import Peer from "simple-peer";
 
-const SocketContext = createContext();
+// const SocketContext = createContext();
+
+const SocketContext = React.createContext({
+  call: {},
+  callAccepted: false,
+  myVideo: null,
+  userVideo: null,
+  stream: null,
+  name: "",
+  setName: (text) => {},
+  callEnded: false,
+  me: "",
+  callUser: (id) => {},
+  leaveCall: () => {},
+  answerCall: () => {},
+});
 
 const socket = io("http://localhost:5000");
 
